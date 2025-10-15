8171f9c9d7c901005ef49ba0556d205c66c2f5e0
Lucciano Scapini <Lucciano.scapini@usach.cl>
2025-10-15 03:13:21 -0300

    Adds Dockerfile and configures Keycloak URL
    Adds a Dockerfile for containerization and deployment.
    
    Configures the Keycloak URL to use an environment variable,
    allowing for dynamic configuration based on the deployment
    environment. This avoids hardcoding the URL and improves
    flexibility.

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,8 +1,9 @@
 import Keycloak from "keycloak-js";
 
+const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL;
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:9090/',
+    url: KEYCLOAK_URL,
     realm: 'ToolRent',
     clientId: 'FrontEnd'
 });
