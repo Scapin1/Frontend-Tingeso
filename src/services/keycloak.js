f652178d6267078193cb960e4cb4c418a2430693
Lucciano Camilo Scapini yañez <lucciano.scapini@usach.cl>
2025-11-20 20:02:32 -0300

    Updates Keycloak URL
    Updates the Keycloak URL to the correct port.
    
    This ensures the application correctly connects to the Keycloak authentication server.

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:9090',
+    url: 'http://localhost:9019',
     realm: 'ToolRent',
     clientId: 'FrontEnd'
 });
