0a39461a89acdb60a645885176f584cbd5c4ee94
Lucciano Camilo Scapini yañez <lucciano.scapini@usach.cl>
2025-11-24 18:02:11 -0300

    Updates Keycloak URL configuration
    Configures the Keycloak URL to use the environment variable `VITE_KEYCLOAK_URL` if available, otherwise defaults to `http://localhost:9090`. This allows for easier configuration in different environments.

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,11 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-<<<<<<< HEAD
-    url: 'http://localhost:9019',
-=======
     url: import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:9090',
->>>>>>> origin/main
     realm: 'ToolRent',
     clientId: 'FrontEnd'
 });
